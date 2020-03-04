9dd53dafd9b6dd04f9e382d6f1c8f6e38b09ec7d
Anierva95 <46223963+Anierva95@users.noreply.github.com>
2020-03-04 18:08:05 -0500

    add burger working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,7 +8,7 @@ const burger = require("../models/burger.js");
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burgers: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
     burger.create([
-    "name", "devoured"
+    "burger_name", "devoured"
   ], [
     req.body.name, req.body.devoured
   ], function(result) {
